9eb48c49a6566c4f1dae09fda1f6825b69ab8c3d
selfishAltruism <officialkyus@gmail.com>
2024-09-22 17:27:47 +0900

    chore: add reveiw type (#25)

--- src/shared/@types/review.d.ts
+++ src/shared/@types/review.d.ts
@@ -1,6 +1,6 @@
 declare namespace Review {
   //DTO
-  export interface Reviews {
+  export interface GetReviewsResDto {
     result: {
       objectives: {
         question: string;
@@ -12,6 +12,20 @@ declare namespace Review {
       };
       repurchase: number[];
       images: { userId: string; imageUrls: string[] }[];
+    };
+  }
+
+  export interface GetReviewResDto {
+    result: {
+      investmentId: string;
+      userId: string;
+      penalty: boolean;
+      answer1: number;
+      answer2: number;
+      answer3: number;
+      answer4: number;
+      answer5: string;
+      answer6: boolean;
     };
   }
 }
---
+++ src/shared/hooks/services/ReviewService.tsx
@@ -0,0 +1,5 @@
+import { API } from "@/shared";
+
+const ReviewService = () => {
+  return {};
+};
