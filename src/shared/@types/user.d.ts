88c253d037dbe9349799052d906528a3b0b698dc
hyeonsoo0625 <hyeonsoo02@gmail.com>
2024-09-28 00:35:18 +0900

    feat: connect signin api

--- src/shared/@types/user.d.ts
+++ src/shared/@types/user.d.ts
@@ -1,18 +1,37 @@
 declare namespace Company {
   //DTO
   export interface SignInReqDto {
-    signId: string;
+    username: string;
     password: string;
   }
 
   export interface SignInResDto {
     result: {
-      id: string;
+      id: number;
       name: string;
       access_token: string;
     };
   }
 
+  export interface SignUpReqDto {
+    username: string;
+    password: string;
+    name: string;
+    regNumber: string;
+    phone: string;
+    email: string;
+    address: string;
+    category: string;
+    size: string;
+    status: string;
+  }
+
+  export interface SignUpResDto {
+    result: {
+      enterpriseId: number;
+      msg: string;
+    };
+  }
   //Store
   export interface CompanyStore {
     id: string;
--- src/shared/hooks/services/AuthService.tsx
+++ src/shared/hooks/services/AuthService.tsx
@@ -5,18 +5,23 @@ import { API, setAccess, storeAccess, useCompanyStore } from "@/shared";
 export const AuthService = () => {
   const setCompany = useCompanyStore((state) => state.setCompany);
 
-  const signIn = async (username: string, password: string) => {
-    const {
-      data: { result },
-    } = (await API.post("/login", {
-      username: username,
-      password: password,
-    })) as AxiosResponse<Company.SignInResDto>;
-
-    setAccess(result.access_token);
-    storeAccess(result.access_token);
-    setCompany(result);
+  const signin = async (body: Company.SignInReqDto) => {
+    const { data } = (await API.post(
+      "/login",
+      body
+    )) as AxiosResponse<Company.SignInResDto>;
+    setAccess(data.result.access_token);
+    storeAccess(data.result.access_token);
+    setCompany(data.result);
+    return data;
   };
 
-  return { signIn };
+  const signup = async (body: Company.SignUpReqDto) => {
+    const { data } = (await API.post(
+      "/signup",
+      body
+    )) as AxiosResponse<Company.SignUpResDto>;
+    return data;
+  };
+  return { signin, signup, };
 };
